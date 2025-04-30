4f765e403d903134016270d22050effc9cd9a30a
andersdm12 <andersdm12@gmail.com>
2025-04-29 18:46:59 -0700

    B: Number of Words

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ def get_book_text(path_to_file):
 		file_contents = f.read()
 	return file_contents
 
+def number_of_words(book_text):
+	words = book_text.split()
+	length = len(words)
+	return length
+
 def main():
 	book_text = get_book_text("books/frankenstein.txt")
-	print(book_text)
+	length = number_of_words(book_text)
+	print(f"{length} words found in the document")
 
 main()
 
