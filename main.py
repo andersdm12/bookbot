6a6abb9becd8ad98f43d2211c43c4befd64d31c8
andersdm12 <andersdm12@gmail.com>
2025-05-02 19:17:53 -0700

    D: implementing different books

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import sys
+
 def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		file_contents = f.read()
 	return file_contents
 
 def main():
-	book_text = get_book_text("books/frankenstein.txt")
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	book_text = get_book_text(sys.argv[1])
 	from stats import number_of_words
 	length = number_of_words(book_text)
 	from stats import count_per_character
@@ -12,7 +17,7 @@ def main():
 	from stats import sort_book
 	sorted_books = sort_book(characters)
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
+	print(f"Analyzing book found at {sys.argv[1]}...")
 	print("----------- Word Count ----------")
 	print(f"Found {length} total words")
 	print("--------- Character Count -------")
@@ -21,6 +26,5 @@ def main():
 	print("============= END ===============")
 
 main()
-
 
 
